c4b1651126292aef5e695dbb194139e5fbb16d36
liam pieri <liamwazherealso@gmail.com>
2014-12-12 11:06:35 -0500

    Added auto-creation of log folder, prompt to create orginization folders,  and recognization of folders

--- siftly.py
+++ siftly.py
@@ -10,30 +10,49 @@ from datetime import datetime
 
 # load stored configuration from the config.json file
 config = json.load(open('config.json'))
-extensions =  config['extensions']
+extensions = config['extensions']
 dwnld = os.path.normpath(config['download_path'])
-host_path  = os.path.normpath(config['host_path'])
+
 
 # logs are stored by the date then the time in the log folder
 current_date = datetime.today().strftime('%d_%m_%y_%H_%M')
+
+# create log folder if it is not there.
+if not os.path.exists('log'):
+    os.makedirs('log')
+
 
 # touch file
 fname = 'log/' + current_date + '.log'
 file = open(fname, 'w')
 file.close()
 
+
 logging.basicConfig(filename=fname, level=logging.INFO)
 
-if (os.path.exists(dwnld)):
-	for key in extensions:
-		for extension in extensions[key]:
-			for file_name in glob.glob(dwnld + '/*' + extension): 	
-				if sys.argv > 1 and '-' == sys.argv[1][0] and 'v' in sys.argv[1]:
-					print 'Moving ' + file_name  + ' to' + host_path + '/' + key
+if os.path.exists(dwnld):
+    for key in extensions:
+        if not os.path.exists(key):
+            if raw_input('Folder ' + key + ' does not exist, would you like to create one? (yes or no) ') == 'y':
+                os.makedirs(key)
+            else:
+                print "Directory not made."
 
-				logging.info('Moving ' + file_name  + ' to' + host_path + '/' + key)
-				shutil.move(file_name,  os.path.join(host_path + '/' + key))	
+    # checks through the files in the dwnld dir to see if they match any of the extensions and moves them.
+    for key in extensions:
+        for extension in extensions[key]:
+            for file_name in glob.glob(dwnld + '/*' + extension):
+                if len(sys.argv) > 1 and 'v' in sys.argv[1]: # verbose
+                    print 'Moving ' + file_name + ' to' + key
+
+                logging.info('Moving ' + file_name + ' to' + key)
+                shutil.move(file_name, key)
+
+    # checking remaining files to determine if they are a folder, I assume folder are applications
+    for file_name in os.listdir(dwnld):
+        if os.path.isdir(os.path.join(dwnld, file_name)) and 'Applications' in extensions:
+            logging.info('Moving ' + file_name + ' to' + extensions['Applications'])
 
 else:
-	print 'Failed to open: ' + dwnld + '\n'
-	dnwld = os.path.normpath(raw_input('Enter a new directory'))
+    print 'Failed to open: ' + dwnld + '\n'
+    dnwld = os.path.normpath(raw_input('Enter a new directory'))
