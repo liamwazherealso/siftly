8e0f5b916af44d83ac71e7c9bd3a456ec2b95e37
liam <liamwazherealso@gmail.com>
2017-06-25 23:52:33 -0400

    Changed to OOP for no good reason.

--- siftly.py
+++ siftly.py
@@ -9,79 +9,62 @@ import sys
 
 from datetime import datetime
 
-"""
-logging - used to make a log file for what happened
-json - Javascript Object Format is used to save what folders extentions should go in
-glob - for string pattern matching with *
-"""
-# load stored configuration from the config.json file
-# Check that the path exists
-if not os.path.exists('./config.json'):
-    # use the basic log file instead
-    shutil.copy('./example_config.json', './config.json')
 
-# JSON is like a dictionary
-config = json.load(open('config.json'))
-# load a list of
-extensions = config['extensions']
-# load the path to download from
-dwnld = os.path.normpath(config['download_path'])
+class sifter(object):
+    def __init__(self):
+        if not os.path.exists('./config.json'):
+            # use the basic log file instead
+            shutil.copy('./example_config.json', './config.json')
 
-# logs are stored by the date then the time in the log folder
-current_date = datetime.today().strftime('%y_%m_%d_%H_%M')
+        self.config = json.load(open('config.json'))
+        self.extensions = self.config['extensions']
+        self.downloadDir = os.path.normpath(self.config['download_path'])
 
-# create log folder if it is not there.
-if not os.path.exists('log'):
-    os.makedirs('log')
+        current_date = datetime.today().strftime('%y_%m_%d_%H_%M')
 
-# touch file
-fname = 'log/' + current_date + '.log'
-file = open(fname, 'w')
-file.close()
+        # setup logging
+        if not os.path.exists('log'):
+            os.makedirs('log')
 
-# set the log to write to the file
-logging.basicConfig(filename=fname, level=logging.INFO)
+        fname = 'log/' + current_date + '.log'
+        file = open(fname, 'w')
+        file.close()
 
-# logs are stored by the date then the time in the log folder
-current_date = datetime.today().strftime('%y_%m_%d_%H_%M')
+        # set the log to write to the file
+        logging.basicConfig(filename=fname, level=logging.INFO)
 
-# touch file
-fname = 'log/' + current_date + '.log'
-file = open(fname, 'w')
-file.close()
+        if not os.path.exists(self.downloadDir):
+            print('Failed to open: ' + self.downloadDir + '\n')   
+            dnwld = os.path.normpath(input('Enter a new directory'))
 
-logging.basicConfig(filename=fname, level=logging.INFO)
+        # iterate through the paths that the files should be going to and check that they exists
+        for key in self.extensions:
+            if not os.path.exists(key):
+                if raw_input('Folder ' + key + ' does not exist, would you like to create one? (yes or no) ') == 'y':
+                    os.makedirs(key)
+                else:
+                    self.extensions.remove(key)
+                    print("Directory not made.")
 
-if os.path.exists(dwnld):
-    # iterate through the paths that the files should be going to and check that they exists
-    for key in extensions:
-        if not os.path.exists(key):
-            if input('Folder ' + key + ' does not exist, would you like to create one? (yes or'
-                                                        ' no) ') == 'y':
-                os.makedirs(key)
-            else:
-                extensions.remove(key)
-                print("Directory not made.")
+    def sift(self):
+        # checks through the files in the downloadDir dir to see if they match any of the self.extensions and moves them.
+        for key in self.extensions:
+            # iterate through the lists of extensions
+            for extension in self.extensions[key]:
+                for file_name in glob.glob(self.downloadDir + '/*' + extension):
 
-    # checks through the files in the dwnld dir to see if they match any of the extensions and moves them.
-    for key in extensions:
-        # iterate through the lists of extentions
-        for extension in extensions[key]:
-            for file_name in glob.glob(dwnld + '/*' + extension):
+                    if len(sys.argv) > 1 and 'v' in sys.argv[1]: # verbose
+                        print('Moving ' + file_name + ' to' + key)
+                    try:
+                        logging.info('Moving ' + file_name + ' to ' + key)
+                        shutil.move(file_name, key)
+                    except:
+                        logging.error("Failed to move " + str(file_name))
 
-                if len(sys.argv) > 1 and 'v' in sys.argv[1]: # verbose
-                    print('Moving ' + file_name + ' to' + key)
-                try:
-                    logging.info('Moving ' + file_name + ' to ' + key)
-                    shutil.move(file_name, key)
-                except:
-                    logging.error("Failed to move " + str(file_name))
+        # checking remaining files to determine if they are a folder, I assume folder are applications
+        for file_name in os.listdir(self.downloadDir):
+            if os.path.isdir(os.path.join(self.downloadDir, file_name)) and 'Applications' in self.extensions:
+                logging.info('Moving ' + file_name + ' to' + self.extensions['Applications'])
 
-    # checking remaining files to determine if they are a folder, I assume folder are applications
-    for file_name in os.listdir(dwnld):
-        if os.path.isdir(os.path.join(dwnld, file_name)) and 'Applications' in extensions:
-            logging.info('Moving ' + file_name + ' to' + extensions['Applications'])
-
-else:
-    print('Failed to open: ' + dwnld + '\n')
-    dnwld = os.path.normpath(input('Enter a new directory'))
+mySifter = sifter()
+mySifter.sift()
